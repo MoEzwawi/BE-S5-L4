bdc74b33e98d4eaa62cf401329c2b1bba764cd48
MoEzwawi <mezwawi96@gmail.com>
2024-01-11 15:23:58 +0100

    first commit

---
+++ src/test/java/MoEzwawi/BES5L4/BeS5L4ApplicationTests.java
@@ -0,0 +1,13 @@
+package MoEzwawi.BES5L4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeS5L4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
